2d666aef5bae88cf78eff73f3b4c30d9d90156ac
ErnestBeckham <ernestbeckham1998@gmail.com>
2024-07-03 22:56:09 +0530

    maddpg replay buffer partially created

---
+++ maddpg.py
@@ -0,0 +1,35 @@
+import os
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import pettingzoo
+from pettingzoo.mpe import simple_adversary_v3
+
+
+
+class MultiAgentReplayBuffer:
+    def __init__(self, max_size, critic_dims, actor_dims, n_actions, n_agents, batch_size):
+        self.mem_size = max_size
+        self.mem_cntr = 0
+        self.n_agents = n_agents
+        self.n_actions = n_actions
+        self.actor_dims = actor_dims
+
+        self.state_memory = np.zeros((self.mem_size, critic_dims))
+        self.new_state_memory = np.zeros((self.mem_size, critic_dims))
+        self.reward_memory = np.zeros((self.mem_size, n_agents))
+        self.terminal_memory = np.zeros((self.mem_size, n_agents), dtype=bool)
+
+        self.init_actor_memory()
+
+    
+    def init_actor_memory(self):
+        self.actor_state_memory = []
+        self.actor_new_state_memory = []
+        self.actor_action_memory = []
+
+        for i in range(self.n_agents):
+            self.actor_state_memory.append(np.zeros((self.mem_size, self.actor_dims[i])))
+            self.actor_new_state_memory.append(np.zeros((self.mem_size, self.actor_dims[i])))
+            self.actor_action_memory.append(np.zeros((self.mem_size, self.n_actions)))
