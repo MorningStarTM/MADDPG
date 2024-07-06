aac576d9da93ef3e9932e2079c28e049dd4a937b
ErnestBeckham <ernestbeckham1998@gmail.com>
2024-07-06 09:22:19 +0530

    critic network created

--- maddpg.py
+++ maddpg.py
@@ -3,6 +3,7 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.optim as optim
 import pettingzoo
 from pettingzoo.mpe import simple_adversary_v3
 
@@ -88,4 +89,30 @@ class MultiAgentReplayBuffer:
 
 
 
+class CriticNetwork(nn.Module):
+    def __init__(self, beta, input_dims, fc1_dims, fc2_dims, n_agents, n_actions, name, chkpt_dir):
+        super(CriticNetwork, self).__init__()
+
+        self.chkpt_dir = os.path.join(chkpt_dir, name)
+        self.fc1 = nn.Linear(input_dims + n_agents*n_actions, fc1_dims)
+        self.fc2 = nn.Linear(fc1_dims, fc2_dims)
+        self.q = nn.Linear(fc2_dims, 1)
+
+        self.optimizer = optim.Adam(self.parameters(), lr=beta)
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.to(device=self.device)
+
+
+    def forward(self, state, action):
+        x = F.relu(self.fc1(torch.cat([state, action], dim=1)))
+        x = F.relu(self.fc2(x))
+        q = self.q(x)
+
+        return q
+    
+    def save_checkpoint(self):
+        torch.save(self.state_dict(), self.chkpt_dir)
+
+    def load_checkpoint(self):
+        self.load_state_dict(torch.load(self.chkpt_dir))
 
