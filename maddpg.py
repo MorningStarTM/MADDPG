e56a0175f6068b197ac21ea99f622fb3cdc3e163
ErnestBeckham <ernestbeckham1998@gmail.com>
2024-07-06 08:43:52 +0530

    function for store transition created

--- maddpg.py
+++ maddpg.py
@@ -33,3 +33,25 @@ class MultiAgentReplayBuffer:
             self.actor_state_memory.append(np.zeros((self.mem_size, self.actor_dims[i])))
             self.actor_new_state_memory.append(np.zeros((self.mem_size, self.actor_dims[i])))
             self.actor_action_memory.append(np.zeros((self.mem_size, self.n_actions)))
+
+
+    def store_transition(self, raw_obs, state, action, reward, raw_obs_, state_, done):
+        if self.mem_cntr % self.mem_size == 0 and self.mem_cntr > 0:
+            self.init_actor_memory()
+        
+        index = self.mem_cntr % self.mem_size
+
+        for agent_idx in range(self.n_agents):
+            self.actor_state_memory[agent_idx][index] = raw_obs[agent_idx]
+            self.actor_new_state_memory[agent_idx][index] = raw_obs_[agent_idx]
+            self.actor_action_memory[agent_idx][index] = action[agent_idx]
+
+        self.state_memory[index] = state
+        self.new_state_memory[index] = state_
+        self.reward_memory[index] = reward
+        self.terminal_memory[index] = done
+
+        self.mem_cntr += 1
+
+
+        
