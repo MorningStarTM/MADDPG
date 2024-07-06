4a84dc98c117be6e78b9b7687e6a562e2264d43d
ErnestBeckham <ernestbeckham1998@gmail.com>
2024-07-06 11:59:45 +0530

    agent initiated

--- maddpg.py
+++ maddpg.py
@@ -145,3 +145,47 @@ class ActorNetwork(nn.Module):
 
     def load_checkpoint(self):
         self.load_state_dict(torch.load(self.chkpt_file))
+
+
+
+class Agent:
+    def __init__(self, actor_dims, critic_dims, n_actions, n_agents, agent_idx, chkpt_dir, alpha=0.01, beta=0.01, fc1=64, fc2=64, gamma=0.95, tau=0.01):
+        self.gamma = gamma
+        self.tau = tau
+        self.n_actions = n_actions
+        self.agent_name = f'agent_{agent_idx}'
+
+        self.actor = ActorNetwork(alpha, actor_dims, fc1, fc2, n_actions, chkpt_dir=chkpt_dir, name=self.agent_name+"_actor")
+        self.critic = CriticNetwork(beta, critic_dims, fc1, fc2, n_agents, n_actions, chkpt_dir=chkpt_dir, name=self.agent_name+"_critic")
+        self.target_actor = ActorNetwork(alpha, actor_dims, fc1, fc2, n_actions, chkpt_dir=chkpt_dir, name=self.agent_name+"_taret_actor")
+        self.target_critic = CriticNetwork(beta, critic_dims, fc1, fc2, n_agents, n_actions, chkpt_dir=chkpt_dir, name=self.agent_name+"_target_critic")
+
+        self.update_network_parameters(tau=1)
+
+
+    def update_network_parameters(self, tau=None):
+        if tau is None:
+            tau = self.tau
+
+        # update actor
+        target_actor_params = self.target_actor.named_parameters()
+        actor_params = self.actor.named_parameters()
+
+        target_actor_state_dict = dict(target_actor_params)
+        actor_state_dict = dict(actor_params)
+
+        for name in actor_state_dict:
+            actor_state_dict[name] = tau * actor_state_dict[name].clone() + (1-tau) * target_actor_state_dict[name].clone()
+        self.target_actor.load_state_dict(actor_state_dict)
+
+
+        #update critic
+        target_critic_params = self.target_critic.named_parameters()
+        critic_params = self.critic.named_parameters()
+
+        target_critic_state_dict = dict(target_critic_params)
+        critic_state_dict = dict(critic_params)
+
+        for name in critic_state_dict:
+            critic_state_dict[name] = tau * critic_state_dict[name].clone() + (1-tau) * target_critic_state_dict[name].clone()
+        self.target_critic.load_state_dict(critic_state_dict)
