70d5b42ff93ae2a1d6483f0d5e353487cf5ec359
ErnestBeckham <ernestbeckham1998@gmail.com>
2024-07-06 11:12:41 +0530

    actor network created

--- maddpg.py
+++ maddpg.py
@@ -116,3 +116,32 @@ class CriticNetwork(nn.Module):
     def load_checkpoint(self):
         self.load_state_dict(torch.load(self.chkpt_dir))
 
+
+class ActorNetwork(nn.Module):
+    def __init__(self, alpha, input_dims, fc1_dims, fc2_dims, n_actions, name, chkpt_dir):
+        super(ActorNetwork, self).__init__()
+
+        self.chkpt_file = os.path.join(chkpt_dir, name)
+
+        self.fc1 = nn.Linear(input_dims, fc1_dims)
+        self.fc2 = nn.Linear(fc1_dims, fc2_dims)
+        self.pi = nn.Linear(fc2_dims, n_actions)
+
+        self.optimizer = optim.Adam(self.parameters(), lr=alpha)
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.to(device=self.device)
+
+    
+    def forward(self, state):
+        x = F.relu(self.fc1(state))
+        x = F.relu(self.fc2(x))
+        pi = torch.softmax(self.pi(x), dim=1)
+
+        return pi
+    
+
+    def save_checkpoint(self):
+        torch.save(self.state_dict(), self.chkpt_file)
+
+    def load_checkpoint(self):
+        self.load_state_dict(torch.load(self.chkpt_file))
